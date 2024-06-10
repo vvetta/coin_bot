f86cacb09fec37d2daa11f6e7de932a2aaf04301
Nikita <109899745+vvetta@users.noreply.github.com>
2024-06-10 17:38:48 +0300

    starter bot

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+load_dotenv()
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_API_TOKEN")).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
